f1c9491a480afb9b6ee7a06004e7d96b657a702f
nicktimbellini <nicholas.bellini@live.com>
2025-03-05 18:19:11 -0700

    This is for mission 06

--- Mission06_Bellini/Controllers/HomeController.cs
+++ Mission06_Bellini/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Mission06_Bellini.Models;
 
 namespace Mission06_Bellini.Controllers
-{
+{// testing
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
