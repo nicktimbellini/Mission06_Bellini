137a0a7857014384ace2c0b0baa4641081174c58
nicktimbellini <nicholas.bellini@live.com>
2025-03-14 23:04:54 -0600

    final

--- Mission06_Bellini/Program.cs
+++ Mission06_Bellini/Program.cs
@@ -11,12 +11,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=movies.db"));
 
 var app = builder.Build();
+
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 app.Run();
